3b5dd19ee4689681b8c3b1d011bd43b43d9e3cc7
j0rdanaaa <j0rdanaaa@gmail.com>
2023-08-19 09:06:14 -0300

    Meu exercício de desing system

---
+++ design-system-meu-exercicio/src/products/product.js
@@ -0,0 +1,28 @@
+export const arrayProducts = [
+  {
+    id: 1,
+    brand: 'TOKSTOK',
+    name: 'Sofá',
+    priceDescount: 'R$1800',
+    priceNormal: 'R$5500',
+    image: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRL1B8mGxuCcPanBwD5v72Zg89rcNKKySkzXHf94uAoHVByiZs35x5u3dybyPGI5OXEfgJI9wpJ5Rik4J2S0mzfLo5fs6jFzLC6FqgVtlWDg8KN1GP1tQsr&usqp=CAc'
+  },
+
+  {
+    id: 2,
+    brand: 'TOKSTOK',
+    name: 'Cadeira',
+    priceDescount: 'R$750',
+    priceNormal: 'R$1500',
+    image: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSqWvuZ4V3vzTXN_Qpn2dCrrsuGArVoRRd6mDGUALZAD83_QjEzyNCaL32HvCOhiMAy3Swx9gHq21eTwveMtwe9g7Nrr_btj2R1WGnZQz4G8l5fRFtxp4Os7Q&usqp=CAc'
+  },
+
+  {
+    id: 3,
+    brand: 'TOKSTOK',
+    name: 'Estante',
+    priceDescount: 'R$2500',
+    priceNormal: 'R$5000',
+    image: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRUB5VNwV3YzpXMQYmYKvDfz5rlxlX89vtJtk9SEc5J1PT_Ym9Dac2-QxHOKuhu8eRmcsihvA4KqkSd3zeLzN3yd4jEkNX2lYXNTAY_ILMPscwviv3cqsnr_A&usqp=CAc'
+  },
+];
